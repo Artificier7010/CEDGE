c7eb460fdd380823e5f0cf5c4d095b624654f1c2
Nik-0977 <gulshanmarkande0soh@gmail.com>
2023-01-17 16:05:50 +0530

    signup added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.scss';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './Pages/Homepage/Homepage';
+import Signup from './Pages/Signuppage/Signup';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Homepage/>}/>
+        <Route path='/Signup' element={<Signup/>}/>
       </Routes>
     </Router>
   );

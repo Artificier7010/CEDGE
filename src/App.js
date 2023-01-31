942885c778dcaab7df33e11c925b0ea4ef7699ef
Nik-0977 <gulshanmarkande0soh@gmail.com>
2023-01-31 22:00:09 +0530

    Privacy and terms

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './Pages/Homepage/Homepage';
 import { useEffect, useState } from 'react';
 import Signup from './Pages/Signuppage/Signup';
+import Privacy from './Pages/Privacypage/Privacy';
+import Terms from './Pages/Termspage/Terms';
 
 function App() {
   const [windowSize, setWindowSize] = useState([
@@ -25,6 +27,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Homepage windowSize={windowSize} />} />
         <Route path='/Signup' element={<Signup windowSize={windowSize}/>}/>
+        <Route path='/Privacy' element={<Privacy windowSize={windowSize}/>}/>
+        <Route path='/Terms' element={<Terms windowSize={windowSize}/>}/>
       </Routes>
     </Router>
   );
